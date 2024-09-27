3b1b1ebcf2a0eaf2cc98358ae1312f7ffbcbce3d
Haidy Saeed <haidy33@gmail.com>
2024-09-27 15:13:40 +0300

    Initial Commit

---
+++ banquemisr.challenge05.Movies_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05.Movies_App
+//
+//  Created by Sohila Ahmed on 27/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
