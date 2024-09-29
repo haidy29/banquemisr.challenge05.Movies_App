4b893d07acb81cd188cbd204585247557143a0fc
Haidy Saeed <haidy33@gmail.com>
2024-09-29 14:47:42 +0300

    design details screen and fetch data using use case

---
+++ banquemisr.challenge05.Movies_App/Moduels/Details/Model/DetailsModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsModel.swift
+//  banquemisr.challenge05.Movies_App
+//
+//  Created by Sohila Ahmed on 29/09/2024.
+//
+
+import Foundation
---
+++ banquemisr.challenge05.Movies_App/UseCaseLayer/DetailsUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsUseCase.swift
+//  banquemisr.challenge05.Movies_App
+//
+//  Created by Sohila Ahmed on 29/09/2024.
+//
+
+import Foundation
